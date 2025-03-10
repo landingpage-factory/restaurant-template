8ff1d5a4753927b996841a1fbfc665c45956a6ab
akocerke <wickedbitcoin@protonmail.com>
2025-03-10 17:56:32 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Für den statischen Export
-  trailingSlash: true,  // Optional: URLs mit einem abschließenden Slash
 };
 
 export default nextConfig;
