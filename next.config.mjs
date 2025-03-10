61d49862b4e466f092e1eea72af3428a4986210b
akocerke <wickedbitcoin@protonmail.com>
2025-03-10 16:31:07 +0100

    update nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Setze `output` auf 'export', um statische HTML-Seiten zu erstellen
+    output: 'export',
+  
+    // Konfiguration für Bilder (Optional: unoptimierte Bilder)
+    images: {
+      unoptimized: true,
+    },
 
-export default nextConfig;
+  };
+  
+  export default nextConfig;
+  
+
